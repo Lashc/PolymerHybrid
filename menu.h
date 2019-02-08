1f0c61b122587e198be0a745f754d6ae05039e0f
Christian Lashover <lashch@att.net>
2019-02-08 00:27:20 -0600

    Added a "Menu" class in an attempt to write the GUI by hand.  The window it produces is similar to the first draft of the main menu.

---
+++ menu.h
@@ -0,0 +1,17 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <QWidget>
+
+class menu : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit menu(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // MENU_H
